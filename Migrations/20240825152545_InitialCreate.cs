d89ed61d67594af9bf5e447816d1bce8bf5eac64
Zev Paneth <z0504167424@gmail.com>
2024-08-25 18:33:15 +0300

    update mission!!

---
+++ Migrations/20240825152545_InitialCreate.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgentManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    x = table.Column<int>(type: "int", nullable: false),
+                    y = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                    table.CheckConstraint("CK_Location_x_Range", "[x] BETWEEN 0 AND 1000");
+                    table.CheckConstraint("CK_Location_y_Range", "[y] BETWEEN 0 AND 1000");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mission",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AgentId = table.Column<int>(type: "int", nullable: false),
+                    TargetId = table.Column<int>(type: "int", nullable: false),
+                    Timer = table.Column<double>(type: "float", nullable: true),
+                    ExecutionTime = table.Column<double>(type: "float", nullable: true),
+                    MissionStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mission", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nickname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AgentStatus = table.Column<int>(type: "int", nullable: false),
+                    photoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agent_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Target",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TargetStatus = table.Column<int>(type: "int", nullable: false),
+                    photoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Target", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Target_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agent_LocationId",
+                table: "Agent",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Target_LocationId",
+                table: "Target",
+                column: "LocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agent");
+
+            migrationBuilder.DropTable(
+                name: "Mission");
+
+            migrationBuilder.DropTable(
+                name: "Target");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+        }
+    }
+}
